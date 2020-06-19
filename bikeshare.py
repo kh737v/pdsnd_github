a35230d8894b85f00b49d048b3c600fbf13610f7
kh737v <kh737v@att.com>
2020-06-19 08:47:31 -0700

    The first change in bikeshare.py for step 4

--- bikeshare.py
+++ bikeshare.py
@@ -9,7 +9,6 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
-
     Returns:
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
@@ -47,7 +46,6 @@ def get_filters():
 def load_data(city, month, day):
     """
     Loads data for the specified city and filters by month and day if applicable.
-
     Args:
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
@@ -240,7 +238,11 @@ def display_data(df):
                 print(df.iloc[:,0:].iloc[firstline:lastline])
             elif select_more.lower() == 'no':
                 break
-    
+def first_commit():
+    print('This is the first commit for step 4. Refactor Code')
+
+
+
 def main():
     while True:
         city, month, day = get_filters()
@@ -250,13 +252,10 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
-        
         display_data(df)
-
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
-
 
 if __name__ == "__main__":
 	main()
